e6f5b14ac8fe8a5a1a14ee68f276ecccee036a96
Mike Freislich <mike.freislich@gmail.com>
2022-01-04 17:49:11 +0200

    simple samples

--- src/sample.h
+++ src/sample.h
@@ -1,33 +1,17 @@
-#ifndef SAMPLE_H 
+#ifndef SAMPLE_H
 #define SAMPLE_H
 #include <Arduino.h>
 
-class Sample {
-    private:
-    uint8_t *data;
-    uint16_t _length;
-    
-    public:
-    Sample() {
+uint16_t getSamples(byte* &data)
+{
+    return sizeof(data);
+}
 
-    }
-    
-    ~Sample() {
-        if (data != NULL)        
-            free(data);
-    }
+// sample position at number of ticks
+uint16_t getSampleAtTick(byte* &data, uint16_t ticks)
+{
+    return 1;
+}
 
-    uint16_t length() {
-        if (_length == 0) _length = sizeof(data);
-        return _length;
-    }
-
-    // sample position at number of ticks
-    static uint16_t position(Sample sample, uint16_t ticks) {
-        return 1;
-    }
-
-
-};
 
 #endif
